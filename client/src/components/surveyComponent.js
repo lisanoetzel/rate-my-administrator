7a5fe6df2ae84eeb6f0dee11a7096dcb688c3a76
Mona Lisa Mameli <lisa.noetzel@gmail.com>
2019-10-24 21:53:07 -0400

    added Home, About, Survey components & routes

---
+++ client/src/components/surveyComponent.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Survey extends Component {
+    render(){
+        return (
+            <div>
+                <p>Welcome to the Survey Component</p>
+            </div>
+        )
+    }
+}
