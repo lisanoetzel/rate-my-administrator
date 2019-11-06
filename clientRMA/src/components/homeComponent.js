9c15e2792082075836d7c7ebc7723acb07e878df
Mona Lisa Mameli <lisa.noetzel@gmail.com>
2019-11-05 21:07:29 -0500

    named all old files with RMA

---
+++ clientRMA/src/components/homeComponent.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class Home extends Component {
+    render(){
+        return (
+            <div>
+                {/* <p>Welcome to the Home Component</p> */}
+                <hr></hr>
+                <h2>Welcome to Rate My Administrator!</h2>
+            </div>
+        )
+    }
+}
